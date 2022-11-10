779437c2d2d57b5ad899428a2cd710312ec778a3
hallo <emial@hallo>
2022-11-10 13:53:52 +0000

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'R4U';
+  author = 'Magnus Freisens';
+  age = '20';
+}
